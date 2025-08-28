9294f1b95862a22f081107bc685f902aaf0800c7
max smith <maxwellfth@gmail.com>
2025-08-28 10:32:04 -0600

    resolve script directory to present working directory and execute creation script from project generation dir

--- init.sh
+++ init.sh
@@ -4,22 +4,21 @@ set -ex
 
 STATUS=0
 
-PROJECT_NAME=${PWD##*/}
-INIT_SCRIPT="$PWD/init.sh"
-PROJECT_ROOT="$PWD"
-PROJECT_GEN_DIR="$PWD/project-generation"
+INIT_SCRIPT=$0                           # init script will be deleted ultimately
+PROJECT_ROOT=$(dirname "$0")             # script dirname may be different than pwd
+cd "$PROJECT_ROOT"                       # ensure pwd is the same as init script
+PROJECT_NAME=${PWD##*/}                  # project name is the same as parent directory
 
-echo "cd to: $PROJECT_GEN_DIR"
-
+# execute project creation script in the same directory as script
+PROJECT_GEN_DIR="$PROJECT_ROOT/project-generation"
 cd "$PROJECT_GEN_DIR"
-bash "create_project.sh $PROJECT_NAME"
+./create_project.sh  "$PROJECT_NAME"
 
-echo "cd back to $PROJECT_ROOT"
+# move generated files to the project root, overwriting readme, .gitignore and any others
 cd "$PROJECT_ROOT"
-
 cp -rf "$PROJECT_GEN_DIR"/generated/"$PROJECT_NAME"/* .
 
-# Cleanup
+# cleanup the project generation resources and scripts
 rm -rf "$PROJECT_GEN_DIR"
 rm "$INIT_SCRIPT"
 
