1c07bad7f1fd3efbcaa91ca5e5d51dfe059ae1f8
max smith <maxwellfth@gmail.com>
2025-08-27 21:59:52 -0600

    delete generated project folder script

---
+++ delete_project.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex
+
+STATUS=0
+
+if [ -n "$1" ];
+  then
+    PROJECT_NAME="$1"
+  else
+    exit 1
+fi
+
+if [ ! -d "$PWD/$PROJECT_NAME" ];
+  then
+    echo "Error: '$PROJECT_NAME' is not a valid directory name in the current path."
+    exit 1
+fi
+
+rm -rf "./$PROJECT_NAME"
+
+exit $STATUS
